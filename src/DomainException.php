149f3bc1bcd5b726657ba068f3b117611cb4d644
neneaX <neneax@gmail.com>
2016-01-09 14:14:31 +0200

    + Added DomainException + Added InvalidArgumentException

---
+++ src/DomainException.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Exception;
+
+/**
+ *
+ * @author Andrei
+ */
+class DomainException extends \DomainException
+{
+
+	/**
+	 * The provided value that caused the exception
+	 *
+	 * @var string
+	 */
+	protected $providedValue = 'unknown';
+
+	/**
+	 * The current domain that should contain the provided value
+	 *
+	 * @var string
+	 */
+	protected $currentDomain = 'unknown';
+
+	/**
+	 * The exception message
+	 *
+	 * @var string
+	 */
+	protected $message = 'The provided value [%s] does not exist or does not belong to the current domain [%s].';
+
+	/**
+	 * The exception code
+	 *
+	 * @var int
+	 */
+	protected $code = 10400;
+
+	/**
+	 * The exception string code
+	 *
+	 * @var string
+	 */
+	protected $stringCode = 'DOMAIN';
+
+	/**
+	 *
+	 * @param string $providedValue The provided value that caused the exception
+	 * @param string $currentDomain The current domain that should contain the provided value
+	 * @param string $previous The previous exception, if any
+	 */
+	public function __construct($providedValue, $currentDomain, $previous = null)
+	{
+		$this->setProvidedValue($providedValue);
+		$this->setCurrentDomain($currentDomain);
+
+		$this->setMessage();
+
+		parent::__construct($this->message, $this->code, $previous);
+	}
+
+	/**
+	 * Set the provided value that caused the exception
+	 *
+	 * @param string $providedValue
+	 */
+	protected function setProvidedValue($providedValue)
+	{
+		if (is_string($providedValue)) {
+			$this->providedValue = $providedValue;
+		} else {
+			$this->providedValue = json_encode($providedValue);
+		}
+	}
+
+	/**
+	 * Set the current domain that should contain the provided value
+	 *
+	 * @param string $currentDomain
+	 */
+	protected function setCurrentDomain($currentDomain)
+	{
+		if (is_string($currentDomain)) {
+			$this->currentDomain = $currentDomain;
+		} else {
+			$this->currentDomain = json_encode($currentDomain);
+		}
+	}
+
+	/**
+	 * Set the additional parameters in the message
+	 */
+	protected function setMessage()
+	{
+		$message = sprintf($this->message, $this->providedValue, $this->currentDomain);
+
+		$this->message = $message;
+	}
+
+}
+
+?>
---
+++ src/InvalidArgumentException.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Exception;
+
+/**
+ *
+ * @author Andrei
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+	/**
+	 * The name of the invalid argument that caused the exception
+	 *
+	 * @var string
+	 */
+	protected $argumentName = 'unknown';
+
+	/**
+	 * The expected type of the invalid argument
+	 *
+	 * @var string
+	 */
+	protected $expected = 'unknown';
+
+	/**
+	 * The actual type of the invalid argument that has been given
+	 *
+	 * @var string
+	 */
+	protected $actual = 'unknown';
+
+	/**
+	 * The exception message
+	 *
+	 * @var string
+	 */
+	protected $message = 'Invalid [%s] argument: [%s] expected - [%s] given.';
+
+	/**
+	 * The exception code
+	 *
+	 * @var int
+	 */
+	protected $code = 10400;
+
+	/**
+	 * The exception string code
+	 *
+	 * @var string
+	 */
+	protected $stringCode = 'INVALID_ARGUMENT';
+
+	/**
+	 *
+	 * @param string $argumentName The name of the invalid argument that caused the exception
+	 * @param string $expected The expected type of the invalid argument
+	 * @param string $actual The actual type of the invalid argument that has been given
+	 * @param string $previous The previous exception, if any
+	 */
+	public function __construct($argumentName, $expected, $actual, $previous = null)
+	{
+		$this->setArgumentName($argumentName);
+		$this->setExpected($expected);
+		$this->setActual($actual);
+
+		$this->setMessage();
+
+		parent::__construct($this->message, $this->code, $previous);
+	}
+
+	/**
+	 * Set the name of the invalid argument that caused the exception
+	 *
+	 * @param string $argumentName
+	 */
+	protected function setArgumentName($argumentName)
+	{
+		if (is_string($argumentName)) {
+			$this->argumentName = $argumentName;
+		} else {
+			$this->argumentName = json_encode($argumentName);
+		}
+	}
+
+	/**
+	 * Set the expected type of the invalid argument
+	 *
+	 * @param string $expected
+	 */
+	protected function setExpected($expected)
+	{
+		if (is_string($expected)) {
+			$this->expected = $expected;
+		} else {
+			$this->expected = json_encode($expected);
+		}
+	}
+
+	/**
+	 * Set the actual type of the invalid argument that has been given
+	 *
+	 * @param string $actual
+	 */
+	protected function setActual($actual)
+	{
+		if (is_string($actual)) {
+			$this->actual = $actual;
+		} else {
+			$this->actual = json_encode($actual);
+		}
+	}
+
+	/**
+	 * Set the additional parameters in the message
+	 */
+	protected function setMessage()
+	{
+		$message = sprintf($this->message, $this->argumentName, $this->expected, $this->actual);
+
+		$this->message = $message;
+	}
+
+}
+
+?>
